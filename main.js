2d1ff659573fe9d88af0489a252bd87a24ec96e6
Alessandro Marega <ale_marega@hotmail.it>
2021-03-12 15:49:20 +0100

    fix sintassi e aggiunti eventi al click sulle icone next e prev

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ $(function() {
 
   // VARIABILI:
   var nextIcon = $(".next");
-
-
+  var prevIcon = $(".prev");
 
   // FUNZIONI:
-  function removeClassActive(element, firstElement) {
+  // funzione che sposta la classe 'active' ai successivi
+  function pushAfterClassActive(element, firstElement) {
 
     element.removeClass("active");
     if (element.hasClass("last") == false) {
@@ -17,12 +17,34 @@ $(function() {
 
   }
 
-  // aggiungo scorrimento al click su icona 'next'
+  // funzione che sposta la classe 'active' ai successivi
+  function pushBeforeClassActive(element, lastElement) {
+
+    element.removeClass("active");
+    if (element.hasClass("first") == false) {
+      element.prev().addClass("active");
+    } else {
+      lastElement.addClass("active");
+    }
+
+  }
+
+
+  // aggiungo evento con scorrimento al click su icona 'next'
   nextIcon.click(
     function () {
       var imgAct = $(".images img.active");
       var firstImg = $(".images img.first");
-      removeClassActive(imgAct, firstImg);
+      pushAfterClassActive(imgAct, firstImg);
+    }
+  );
+
+  // aggiungo evento con scorrimento al click su icona 'prev'
+  prevIcon.click(
+    function () {
+      var imgAct = $(".images img.active");
+      var lastImg = $(".images img.last");
+      pushBeforeClassActive(imgAct, lastImg);
     }
   );
 
