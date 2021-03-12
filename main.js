2f6162c2a4e6e08c286ae61a20dd2b415a8b4a1c
Alessandro Marega <ale_marega@hotmail.it>
2021-03-12 15:37:42 +0100

    aggiunto scorrimento al click su icona 'next'

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-$(funcion() {
+$(function() {
+
+  // VARIABILI:
+  var nextIcon = $(".next");
 
 
+
+  // FUNZIONI:
+  function removeClassActive(element, firstElement) {
+
+    element.removeClass("active");
+    if (element.hasClass("last") == false) {
+      element.next().addClass("active");
+    } else {
+      firstElement.addClass("active");
+    }
+
+  }
+
+  // aggiungo scorrimento al click su icona 'next'
+  nextIcon.click(
+    function () {
+      var imgAct = $(".images img.active");
+      var firstImg = $(".images img.first");
+      removeClassActive(imgAct, firstImg);
+    }
+  );
 
 });
